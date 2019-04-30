2fe17a3f119ba0d727bab6290deb4e783beb1e7a
Erik Young <youngblkraven@gmail.com>
2019-04-30 08:03:49 -0600

    snap 4-25

--- snap-4-25/snap-4-25.php
+++ snap-4-25/snap-4-25.php
@@ -3,24 +3,24 @@
 	 * Doing a snap challenge in which we need to write a class with two state variables, accessors, mutators, and a method that does something
 	 */
 
-		class dog {
+		class DOG {
 			/** color of the dog
 			 * @var string $dogColor
 			 */
-			public $dogColor;
+			public $color;
 			/** breed of the dog
 			 * @var string $dogBreed
 			 */
-			public $dogBreed;
+			public $breed;
+
 
 			public function getDogColor() {
-		return ($this->dogColor);
+		return ($this->color);
 	}
-	public function getDogBreed() {
-		return ($this->dogBreed);
+	public function getBreed() {
+		return ($this->breed);
 		}}
-
-	/**
+		/**
 	 * constructor for dog
 	 *
 	 * @param string $newDogColor provide the color of the dog
